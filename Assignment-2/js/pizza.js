78ecd81b60c73fe9cb72d38f4208c484d908cb52
Keifer Grainger <200488755@student.georgianc.on.ca>
2026-07-28 20:40:52 -0400

    Add header comments for project documentation and author details

---
+++ Assignment-2/js/pizza.js
@@ -0,0 +1,5 @@
+/* ============================================================
+   THE PIZZA MAKER
+   Author: Keifer Grainger  (Student ID: 200488755)
+   Course: COMP1073 - Client-Side JavaScript
+   ============================================================ */
