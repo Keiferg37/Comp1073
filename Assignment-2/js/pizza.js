818b0ac10b7338b0d12d867be4dc4889bce7e869
Keifer Grainger <200488755@student.georgianc.on.ca>
2026-07-28 20:41:26 -0400

    Add dynamic student information display to the pizza maker

--- Assignment-2/js/pizza.js
+++ Assignment-2/js/pizza.js
@@ -3,3 +3,7 @@
    Author: Keifer Grainger  (Student ID: 200488755)
    Course: COMP1073 - Client-Side JavaScript
    ============================================================ */
+
+// STEP 1: Dynamically add the student id and name to the page
+const student = document.querySelector("#student");
+student.textContent = "Keifer Grainger | Student ID: 200488755";
